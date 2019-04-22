27bacbd2a15060953c32f731d66b86ce6c55a0a8
ThatOneCheetah <yw.asus@hotmail.com>
2019-04-22 18:05:21 +0200

    Initial commit

---
+++ src/LTP.Interop.NetStandard/InteropServices/LibraryLoader.cs
@@ -0,0 +1,104 @@
+﻿#region Using statements
+using System;
+using System.IO;
+using System.Security;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+
+using LTP.Interop.IO;
+#endregion
+
+namespace LTP.Interop.InteropServices
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[SuppressUnmanagedCodeSecurity]
+	public static class LibraryLoader
+	{
+		#region DllImport
+		[DllImport( "kernel32" )]
+		private static extern IntPtr LoadLibrary( [In] [MarshalAs( UnmanagedType.LPStr )] string lpFileName );
+
+		[DllImport( "kernel32" )]
+		private static extern IntPtr GetProcAddress( IntPtr hModule, [In] [MarshalAs( UnmanagedType.LPStr )] string lpProcName );
+
+		[DllImport( "libdl" )]
+		private static extern IntPtr dlopen( [In] [MarshalAs( UnmanagedType.LPStr )] string filename, int flags );
+
+		[DllImport( "libdl" )]
+		private static extern IntPtr dlsym( IntPtr handle, [In] [MarshalAs( UnmanagedType.LPStr )] string symbol );
+
+		#region dlopen flags
+		private const int RTLD_NOW = 2;
+		private const int RTLD_GLOBAL = 256;
+		#endregion
+		#endregion
+
+		#region Fields / Properties
+		private static Type _typeofDelegate = typeof( MulticastDelegate );
+		#endregion
+
+		#region Delegates
+		public delegate IntPtr OpenHandler( string filename, int flags );
+		public delegate IntPtr SymbolHandler( IntPtr handle, string symbol );
+		#endregion
+
+		#region Constructors
+		static LibraryLoader()
+		{
+			if( PlatformInformation.Platform == OSPlatform.Windows )
+			{
+				Open += ( string filename, int flags ) => LoadLibrary( filename );
+				Symbol += GetProcAddress;
+			}
+			else if( PlatformInformation.Platform == OSPlatform.Linux || PlatformInformation.Platform == OSPlatform.OSX )
+			{
+				Open += dlopen;
+				Symbol += dlsym;
+			}
+		}
+		#endregion
+
+		#region Methods
+		public static OpenHandler Open { get; }
+		public static SymbolHandler Symbol { get; }
+
+		public static IntPtr Load( Type ofType, bool rtldGlobal = true )
+		{
+			object[] attributes = ofType.GetCustomAttributes( typeof( LibraryAttribute ), false );
+			if( attributes.Length == 0 ) throw new ArgumentNullException( "Type '" + ofType.Name + "' is missing LibraryAttribute." );
+
+			#region Find library
+			LibraryAttribute libAttribute = (LibraryAttribute)attributes[ 0 ];
+			string filename = LibraryFinder.Locate( libAttribute.Name, libAttribute.SearchOptions, libAttribute.CustomRoot );
+			#endregion
+
+			#region Open library
+			IntPtr libraryPtr = IntPtr.Zero;
+
+			if( !File.Exists( filename ) )
+				throw new FileNotFoundException( filename );
+
+			libraryPtr = Open( filename, rtldGlobal ? RTLD_NOW : RTLD_GLOBAL | RTLD_NOW );
+
+#if DEBUG
+			ConsoleColor oldFG = Console.ForegroundColor;
+
+			Console.ForegroundColor = ConsoleColor.DarkGreen;
+			Console.WriteLine( filename );
+			Console.WriteLine( "\t|" );
+			Console.WriteLine( "\t-> (0x{0})", libraryPtr.ToString( "X16" ) );
+			Console.WriteLine();
+			Console.ForegroundColor = oldFG;
+#endif
+			#endregion
+
+			CilHelper.LinkAllExternalMethods( ofType, libraryPtr );
+
+			return libraryPtr;
+		}
+		#endregion
+	}
+}
---
+++ src/LTP.Interop.NetStandard/InteropServices/Memory.cs
@@ -0,0 +1,86 @@
+﻿#region Using statements
+using System.Runtime.InteropServices;
+#endregion
+
+namespace LTP.Interop.InteropServices
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public static unsafe class Memory
+	{
+		#region DllImport
+		// msvcrt
+		[DllImport( "msvcrt" )]
+		private static extern void* msvcrt_memchr( void* ptr, int value, uint n );
+
+		[DllImport( "msvcrt" )]
+		private static extern int msvcrt_memcmp( void* ptr1, void* ptr2, uint n );
+
+		[DllImport( "msvcrt" )]
+		private static extern void msvcrt_memcpy( void* dest, void* src, uint n );
+
+		[DllImport( "msvcrt" )]
+		private static extern void msvcrt_memmove( void* dest, void* src, uint n );
+
+		[DllImport( "msvcrt" )]
+		private static extern void msvcrt_memset( void* s, byte c, uint n );
+
+		// libc
+		[DllImport( "libc" )]
+		private static extern void* libc_memchr( void* ptr, int value, uint n );
+
+		[DllImport( "libc" )]
+		private static extern int libc_memcmp( void* ptr1, void* ptr2, uint n );
+
+		[DllImport( "libc" )]
+		private static extern void libc_memcpy( void* dest, void* src, uint n );
+
+		[DllImport( "libc" )]
+		private static extern void libc_memmove( void* dest, void* src, uint n );
+
+		[DllImport( "libc" )]
+		private static extern void libc_memset( void* ptr, byte chr, uint n );
+		#endregion
+
+		#region Delegates
+		public delegate void* CharacterHandler( void* ptr, int value, uint n );
+		public delegate int CompareHandler( void* ptr1, void* ptr2, uint n );
+		public delegate void CopyHandler( void* dest, void* src, uint n );
+		public delegate void MoveHandler( void* dest, void* src, uint n );
+		public delegate void SetHandler( void* ptr, byte chr, uint n );
+		#endregion
+
+		#region Constructors
+		static Memory()
+		{
+			OSPlatform platform = PlatformInformation.Platform;
+
+			if( platform == OSPlatform.Windows )
+			{
+				Character += msvcrt_memchr;
+				Compare += msvcrt_memcmp;
+				Copy += msvcrt_memcpy;
+				Move += msvcrt_memmove;
+				Set += msvcrt_memset;
+			}
+			else if( platform == OSPlatform.Linux || platform == OSPlatform.OSX )
+			{
+				Character += libc_memchr;
+				Compare += libc_memcmp;
+				Copy += libc_memcpy;
+				Move += libc_memmove;
+				Set += libc_memset;
+			}
+		}
+		#endregion
+
+		#region Methods
+		public static CharacterHandler Character;
+		public static CompareHandler Compare;
+		public static CopyHandler Copy;
+		public static MoveHandler Move;
+		public static SetHandler Set;
+		#endregion
+	}
+}
