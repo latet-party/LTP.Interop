a2816ca997afbe21265ebf047609731410f40124
ThatOneCheetah <yw.asus@hotmail.com>
2019-05-20 18:03:18 +0200

    Fixed missing EntryPoint fields in Memory.cs

--- src/LTP.Interop.NetStandard/InteropServices/Memory.cs
+++ src/LTP.Interop.NetStandard/InteropServices/Memory.cs
@@ -11,36 +11,36 @@ namespace LTP.Interop.InteropServices
 	{
 		#region DllImport
 		// msvcrt
-		[DllImport( "msvcrt" )]
-		private static extern void* msvcrt_memchr( void* ptr, int value, uint n );
+		[DllImport( "msvcrt", EntryPoint = "memchr" )]
+		private static extern void* MSVCRT_MemoryCharacter( void* ptr, int value, uint n );
 
-		[DllImport( "msvcrt" )]
-		private static extern int msvcrt_memcmp( void* ptr1, void* ptr2, uint n );
+		[DllImport( "msvcrt", EntryPoint = "memcmp" )]
+		private static extern int MSVCRT_MemoryCompare( void* ptr1, void* ptr2, uint n );
 
-		[DllImport( "msvcrt" )]
-		private static extern void msvcrt_memcpy( void* dest, void* src, uint n );
+		[DllImport( "msvcrt", EntryPoint = "memcpy" )]
+		private static extern void MSVCRT_MemoryCopy( void* dest, void* src, uint n );
 
-		[DllImport( "msvcrt" )]
-		private static extern void msvcrt_memmove( void* dest, void* src, uint n );
+		[DllImport( "msvcrt", EntryPoint = "memmove" )]
+		private static extern void MSVCRT_MemoryMove( void* dest, void* src, uint n );
 
-		[DllImport( "msvcrt" )]
-		private static extern void msvcrt_memset( void* s, byte c, uint n );
+		[DllImport( "msvcrt", EntryPoint = "memset" )]
+		private static extern void MSVCRT_MemorySet( void* s, byte c, uint n );
 
 		// libc
-		[DllImport( "libc" )]
-		private static extern void* libc_memchr( void* ptr, int value, uint n );
+		[DllImport( "libc", EntryPoint = "memchr" )]
+		private static extern void* LibC_MemoryCharacter( void* ptr, int value, uint n );
 
-		[DllImport( "libc" )]
-		private static extern int libc_memcmp( void* ptr1, void* ptr2, uint n );
+		[DllImport( "libc", EntryPoint = "memcmp" )]
+		private static extern int LibC_MemoryCompare( void* ptr1, void* ptr2, uint n );
 
-		[DllImport( "libc" )]
-		private static extern void libc_memcpy( void* dest, void* src, uint n );
+		[DllImport( "libc", EntryPoint = "memcpy" )]
+		private static extern void LibC_MemoryCopy( void* dest, void* src, uint n );
 
-		[DllImport( "libc" )]
-		private static extern void libc_memmove( void* dest, void* src, uint n );
+		[DllImport( "libc", EntryPoint = "memmove" )]
+		private static extern void LibC_MemoryMove( void* dest, void* src, uint n );
 
-		[DllImport( "libc" )]
-		private static extern void libc_memset( void* ptr, byte chr, uint n );
+		[DllImport( "libc", EntryPoint = "memset" )]
+		private static extern void LibC_MemorySet( void* ptr, byte chr, uint n );
 		#endregion
 
 		#region Delegates
@@ -58,19 +58,19 @@ namespace LTP.Interop.InteropServices
 
 			if( platform == OSPlatform.Windows )
 			{
-				Character += msvcrt_memchr;
-				Compare += msvcrt_memcmp;
-				Copy += msvcrt_memcpy;
-				Move += msvcrt_memmove;
-				Set += msvcrt_memset;
+				Character += MSVCRT_MemoryCharacter;
+				Compare += MSVCRT_MemoryCompare;
+				Copy += MSVCRT_MemoryCopy;
+				Move += MSVCRT_MemoryMove;
+				Set += MSVCRT_MemorySet;
 			}
 			else if( platform == OSPlatform.Linux || platform == OSPlatform.OSX )
 			{
-				Character += libc_memchr;
-				Compare += libc_memcmp;
-				Copy += libc_memcpy;
-				Move += libc_memmove;
-				Set += libc_memset;
+				Character += LibC_MemoryCharacter;
+				Compare += LibC_MemoryCompare;
+				Copy += LibC_MemoryCopy;
+				Move += LibC_MemoryMove;
+				Set += LibC_MemorySet;
 			}
 		}
 		#endregion
